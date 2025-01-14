d60cce94bb241b57a4e57464d28d32114cd40207
Asmatzaile <gorkainventor@gmail.com>
2025-01-14 14:48:44 +0100

    add library of draggable sounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 
 function App() {
+  const soundClasses = [0,1,2,3,4,5,6].map((soundClass)=> <SoundClass soundClass={soundClass} createSoundInstance={(pos)=>addSoundInstance({soundClass, pos})} /> )
 
-  return (
-    <div className="flex justify-center items-center min-h-svh">
-      <h1 className>Soundpot</h1>
-      <Pot />
-    </div>
-  )
-}
-
-const Pot = () => {
   const [highestZIndex, setHighestZIndex] = useState(1);
   const getHigherZIndex = (zIndex) => {
     const newHighestZIndex = (zIndex >= highestZIndex) ? zIndex : highestZIndex + 1;
@@ -20,12 +12,30 @@ const Pot = () => {
     return newHighestZIndex;
   }
 
+  const [soundInstancesData, setSoundInstancesData] = useState([]);
+  const addSoundInstance = (instanceData) => setSoundInstancesData([...soundInstancesData, instanceData]);
+
+  return (
+    <main className="min-h-dvh grid grid-cols-[4fr_minmax(200px,_1fr)] touch-none">
+      <Pot soundInstances={soundInstancesData.map(instanceData=> <SoundInstance soundClass={instanceData.soundClass} pos={instanceData.pos} getHigherZIndex={getHigherZIndex} />)}/>
+      <Library soundClasses={soundClasses} addSoundInstance={addSoundInstance} />
+    </main>
+  )
+}
+
+const Library = ({ soundClasses }) => {
+  return (
+    <div id="library" className="bg-stone-800 p-4 grid grid-cols-[repeat(auto-fill,_minmax(64px,_1fr))] content-start place-items-center gap-4">
+      {soundClasses}
+    </div>
+  )
+}
+
+const Pot = ({ soundInstances }) => {
   return(
-    <>
-      <SoundObject getHigherZIndex={getHigherZIndex} />
-      <SoundObject getHigherZIndex={getHigherZIndex} />
-      <SoundObject getHigherZIndex={getHigherZIndex} />
-    </>
+    <div id="pot" className="bg-stone-900">
+      {soundInstances}
+    </div>
   )
 }
 
@@ -34,18 +44,47 @@ const getBorderColor = () => {
  return borderColorNames[Math.floor(Math.random()*borderColorNames.length)];
 }
 
-const SoundObject = ({ getHigherZIndex }) => {
-  const borderColor = useRef(getBorderColor())
+const SoundClass = ( { soundClass, createSoundInstance }) => {
+  const divRef = useRef(null);
+  const borderColor = useRef(borderColorNames[soundClass]);
+
+  return (
+    <div ref={divRef} className={`w-16 h-16 border-8 rounded-full ${borderColor.current} cursor-grab touch-none`}
+      onPointerDown={(e) => createSoundInstance({x0: getElementCenter(divRef.current).x, y0: getElementCenter(divRef.current).y, mx0: e.clientX, my0: e.clientY})}/>
+  )
+}
+
+const SoundInstance = ({ soundClass, getHigherZIndex, pos }) => {
+  const borderColor = useRef(borderColorNames[soundClass]);
   const [zIndex, setZIndex] = useState(0);
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
-  const bind = useDrag(({ first, offset: [x, y] }) => {
-    api.start({ x, y });
+
+  const [dragging, setDragging] = useState(false)
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0}))
+  const bind = useDrag(({ active, first, offset: [x, y] }) => {
     if (first) setZIndex(getHigherZIndex(zIndex));
+    setDragging(active);
+    api.start({ x, y });
   })
 
-  return <animated.div {...bind()}
-    className={`absolute w-24 h-24 border-8 rounded-full ${borderColor.current} cursor-pointer touch-none`}
-    style={{ x, y, zIndex }} />
+  const divRef = useRef(null);
+  useEffect(() => {
+    const pointerEvent = new PointerEvent("pointerdown", {buttons: 1, clientX: pos.mx0, clientY: pos.my0, bubbles:true})
+    divRef.current.dispatchEvent(pointerEvent);
+  }, []);
+
+  const left = pos.x0-48; // 96 px wide
+  const top = pos.y0-48; // 96 px tall
+  return <animated.div {...bind()} ref={divRef}
+    className={`absolute w-24 h-24 border-8 rounded-full ${borderColor.current} ${dragging ? 'cursor-grabbing' : 'cursor-grab'} touch-none`}
+    style={{ x, y, zIndex, left, top }} />
+}
+
+
+const getElementCenter = (element) => {
+  const rect = element.getBoundingClientRect();
+  const centerX = rect.left + rect.width / 2;
+  const centerY = rect.top + rect.height / 2;
+  return {x: centerX, y: centerY}
 }
 
 export default App
