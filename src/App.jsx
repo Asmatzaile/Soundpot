7c312edbf675588f7ae4f69640bda19fe182498a
Asmatzaile <gorkainventor@gmail.com>
2025-01-15 20:00:18 +0100

    add instance merging on collision

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 
@@ -18,19 +18,40 @@ function App() {
     const key = getNewInstanceKey();
     setSoundInstancesData(new Map(soundInstancesData.set(key, instanceData)));
   }
+  const removeSoundInstance = (key) => {
+    soundInstancesData.delete(key);
+    setSoundInstancesData(new Map(soundInstancesData));
+  }
+  const mergeSoundInstances = (key1, key2) => {
+    const {soundClass: soundClass1, pos: pos1} = soundInstancesData.get(key1);
+    const {soundClass: soundClass2, pos: pos2} = soundInstancesData.get(key2);
+    const soundClass = soundClass1 + soundClass2;
+    const pos = {x: (pos1.x + pos2.x) / 2, y: (pos1.y + pos2.y) / 2};
+    addSoundInstance({soundClass, pos});
+    removeSoundInstance(key1);
+    removeSoundInstance(key2);
+  }
 
   const [highestZIndex, setHighestZIndex] = useState(1);
-
   const instanceFunctions = {
-    removeInstance: (key) => {
-      soundInstancesData.delete(key);
-      setSoundInstancesData(new Map(soundInstancesData));
-    },
+    removeInstance: (key) => removeSoundInstance(key),
     getHigherZIndex: (zIndex) => {
       const newHighestZIndex = (zIndex >= highestZIndex) ? zIndex : highestZIndex + 1;
       setHighestZIndex(newHighestZIndex);
       return newHighestZIndex;
     },
+    updatePos: (key, [newX, newY]) => {
+      soundInstancesData.set(key, {...soundInstancesData.get(key), pos: {x: newX, y: newY}})
+      setSoundInstancesData(new Map(soundInstancesData));
+    },
+    checkMerges: (key) => {
+      const pos = soundInstancesData.get(key).pos;
+      const collidingKey = [...soundInstancesData.keys()].find(candidateKey => {
+        const candidatePos = soundInstancesData.get(candidateKey).pos;
+        return key != candidateKey && doCirclesCollide(pos.x, pos.y, 48, candidatePos.x, candidatePos.y, 48);
+      });
+      if (collidingKey !== undefined) mergeSoundInstances(key, collidingKey)
+    }
   }
 
   const soundInstances = [...soundInstancesData.entries()].map(([key, instanceData]) => <SoundInstance key={key} id={key} functions={instanceFunctions} soundClass={instanceData.soundClass} pos={instanceData.pos} />);
@@ -65,7 +86,7 @@ const getBorderColor = () => {
 
 const SoundClass = ( { soundClass, createSoundInstance }) => {
   const divRef = useRef(null);
-  const borderColor = useRef(borderColorNames[soundClass]);
+  const borderColor = useRef(borderColorNames[soundClass%borderColorNames.length]);
 
   const handlePointerDown = (e) => {
     createSoundInstance({x: getElementCenter(divRef.current).x, y: getElementCenter(divRef.current).y});
@@ -80,14 +101,26 @@ const SoundClass = ( { soundClass, createSoundInstance }) => {
 }
 
 const SoundInstance = ({ id, soundClass, pos, functions }) => {
-  const borderColor = useRef(borderColorNames[soundClass]);
+  const borderColor = useRef(borderColorNames[soundClass%borderColorNames.length]);
   const [zIndex, setZIndex] = useState(0);
 
   const [dragging, setDragging] = useState(false)
-  const [{ x, y }, api] = useSpring(() => ({ x: pos.x, y: pos.y}))
+  const onChangeRef = useRef(null);
+  const [{ x, y }, api] = useSpring(() => ({ x: pos.x, y: pos.y, onChange: () => onChangeRef?.current()}))
+
+  // needed because Spring memoizes everything
+  useEffect(()=> {
+    onChangeRef.current = () => {
+      functions.updatePos(id, [x.get(), y.get()]);
+      if (!dragging) functions.checkMerges(id);
+    }
+  }, [dragging, functions]) // functions is also a dep cause it relies on current values of soundInstancesData
+
+
   const bind = useDrag(({ active, first, last, xy, offset: [x, y] }) => {
     if (first) setZIndex(functions.getHigherZIndex(zIndex));
     if (last) {
+      functions.checkMerges(id);
       const elems = document.elementsFromPoint(xy[0], xy[1]); // thanks https://github.com/pmndrs/use-gesture/issues/88#issuecomment-1154734405
       if (elems && !elems.some(elem=>elem.id=="pot")) functions.removeInstance(id);
     }
@@ -115,6 +148,14 @@ const createPointerDownEvent = (mx, my) => {
     const el = document.elementFromPoint(mx, my);
     const pointerEvent = new PointerEvent("pointerdown", {buttons: 1, clientX: mx, clientY: my, bubbles:true})
     el.dispatchEvent(pointerEvent);
+}
+
+// https://www.jeffreythompson.org/collision-detection/circle-circle.php
+const doCirclesCollide = (c1x, c1y, c1r, c2x, c2y, c2r) => {
+  const dx = c2x - c1x;
+  const dy = c2y - c1y;
+  const d = Math.sqrt(dx*dx + dy*dy);
+  return d <= c1r + c2r;
 }
 
 export default App
