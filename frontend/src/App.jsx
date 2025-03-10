ef1bd177dd93d04ca04881d0150753600d81586c
Asmatzaile <gorkainventor@gmail.com>
2025-03-10 16:11:26 +0100

    clip overflow of app
    so that instances when dragged out of bounds don't create scrollbars

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <SettingsProvider>
-      <main className="h-dvh w-dvw grid grid-cols-[4fr_minmax(270px,1fr)] touch-none">
+      <main className="h-dvh w-dvw grid grid-cols-[4fr_minmax(270px,1fr)] touch-none overflow-clip">
         <LibraryContext.Provider value={{ library: library.data, addSoundToLibrary: library.upload, removeSound, DISPLAYBUFFER_SIZE: library.DISPLAYBUFFER_SIZE }}>
           <Pot instanceManager={instanceManager} openSettings={() =>setIsSettingsOpen(true)}/>
           <Sidebar instanceManager={instanceManager} />
